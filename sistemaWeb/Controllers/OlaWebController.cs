67a8c339fc086e3deafb6108ac1dd56d6c58f2af
pablo <pablo@LAPTOP-OU8BFDKA>
2022-05-22 17:29:14 -0300

    Add project files.

---
+++ sistemaWeb/Controllers/OlaWebController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace sistemaWeb.Controllers
+{
+    public class OlaWebController : Controller
+    {
+        // GET: OlaWeb
+        public string Index()
+        {
+            return "Sistema de cadastro para Web";
+        }   
+
+          //  
+            // GET: /OlaWeb/Bem vindo a Web;
+
+        public string Sistema(string name, int numTimes = 1) { 
+            return HttpUtility.HtmlEncode( "Olá" + name + ", NumTimes is:"+ numTimes);
+        }
+    }
+}
